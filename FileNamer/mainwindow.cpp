781c84b1ae842abf3d97f93af5e35d6b7ac4b049
bluejellybean <alexbarkell@gmail.com>
2013-11-28 15:57:24 -0500

    Added ability to grab file directory
    Currently only adds path to a string

--- FileNamer/mainwindow.cpp
+++ FileNamer/mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
+#include <QtWidgets>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    //QString x = "";
     ui->setupUi(this);
+  //  connect (ui->pushButton,SIGNAL(clicked()), ui->label,SLOT(setText(QString)));
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString Path = QFileDialog::getExistingDirectory (this,
+                                          tr ("Open Directory"), "/",
+                                              QFileDialog::ShowDirsOnly |
+                                          QFileDialog::DontResolveSymlinks);
+    ui->label->setText(Path);
+
 }
 
 MainWindow::~MainWindow()
--- FileNamer/mainwindow.h
+++ FileNamer/mainwindow.h
@@ -14,6 +14,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void setExistingDirectory();
+
+private slots:
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
