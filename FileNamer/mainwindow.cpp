50f0042028ff051f84137119f5d690510629e6bf
bluejellybean <alexbarkell@gmail.com>
2013-11-28 22:51:43 -0500

    Added numbering, hardcoded

--- FileNamer/mainwindow.cpp
+++ FileNamer/mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <iostream>
 #include <QFileDialog>
 
+//#include <stdlib.h>
 //#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -16,14 +17,31 @@ MainWindow::MainWindow(QWidget *parent) :
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString Path = QFileDialog::getExistingDirectory (this,
+    QString path = QFileDialog::getExistingDirectory (this,
                                           tr ("Open Directory"), "/",
                                               QFileDialog::ShowDirsOnly |
                                           QFileDialog::DontResolveSymlinks);
-    ui->label->setText(Path);
+  //  ui->label->setText(path);
     //Renames a hardcoded filename
     //TODO: change this to work with multiple names/file types
-    QFile::rename(Path+"/One.jpg", Path+"/newOne");
+    QDir dir = path;
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting( QDir::Reversed);
+
+    QFileInfoList list = dir.entryInfoList();
+    for (int i = 0; i < list.size(); ++i) {
+        QFileInfo fileInfo = list.at(i);
+
+        QString fileInfosName = fileInfo.fileName();
+        ui->label->setText(fileInfosName);
+
+        QString qStr = QString::number(i);
+
+        QFile::rename(path+"/"+fileInfosName, path+"/"+qStr+"newOne.jpg");
+
+    }
+
+
 
 }
 
